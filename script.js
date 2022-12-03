3bfc4e75e979567be3fb705a77c74509d31470b1
Soeler <k.a.s@hotmail.de>
2022-12-03 22:41:18 +0100

    change stuff

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d4c04b8c33cf4ee8281a5f7d19715b2580053349384f5ffa89f72869bea4e6e6
-size 1704
+(function () {
+    var messages = [
+        "Habt Sonne im Herzen und Pizza im Bauch, <br> dann bleibt ihr glücklich und satt seid ihr auch. Alles Gute zum Hochzeitstag!",
+        "Die Ehe ist etwas für Entdecker und Wagemutige! <br>Diese Voraussetzung habt ihr bereits erfüllt. Alles Gute zum Hochzeitstag!",
+        "Der Heiratsmarkt hat vor … Jahre zwei seiner besten Leute verloren! <br> Das ist das Beste, was euch passieren konnte! Gratulation zum Hochzeitstag!",
+        "Zuerst wart ihr Freunde, dann Verliebte und jetzt Geliebte! Läuft bei euch! <br>Herzlichen Glückwunsch zum Hochzeitstag!",
+        "Ehe bedeutet Gemeinsamkeit und doch wünsche ich euch zum Hochzeitstag auch Trennendes, <br>denn so wird es niemals langweilig.",
+        "Wir wünschen Euch für die Zukunft ganz viel Liebe und ganz viel Humor <br> dann kommt die Gesundheit von ganz alleine! Alles Gute zum Hochzeitstag!",
+        "Alles Gute und alles Liebe wurde euch sicher schon gesagt. Macht einfach weiter so, wir wollen schließlich noch viele Hochzeitstage mit euch feiern!",
+        "Eine gute Ehe ist wie Haarspray: Perfekter Halt ohne zu kleben! <br> Bleibt so, wie ihr seid <br> alles Gute zum Hochzeitstag!",
+        "Aus Liebe macht man komische Sachen – manche heiraten sogar! <br> Gut, dass ihr das gemacht habt. Macht weiter so, ihr Komiker – alles Gute zum Hochzeitstag!",
+        "Heirat ist eine Dummheit, die immer zu zweit begangen wird. Macht einfach weiter das Beste draus <br> alles Liebe zum Hochzeitstag!"
+    ],
+    i = 0;
+    setInterval(function(){ $('#messages').fadeOut(function(){
+        $(this).html(messages[(i = (i + 1) % messages.length)]).fadeIn();
+      }); }, 3000)
+  })();
+  
